0f18b99420457f52cd6e35e165c93611285a21e8
Sourav Datta <souravdatta1995@gmail.com>
2024-05-11 08:59:21 +0530

    dir changes

---
+++ reverseLinkedList.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println()
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	return head
+}
